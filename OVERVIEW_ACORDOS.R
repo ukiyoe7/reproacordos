1fb5ea0ae74520ff98b8c1711fa285a0ec1fe4b5
Sandro Jk <sandro007@gmail.com>
2022-05-29 11:00:54 -0300

    OK

--- OVERVIEW_ACORDOS.R
+++ OVERVIEW_ACORDOS.R
@@ -1,10 +1,17 @@
+## OVERVIEW ACORDOS
+## SANDRO JAKOSKA
+
 library(DBI)
 library(tidyverse)
 
 con3 <- dbConnect(odbc::odbc(), "repro_prod", timeout = 10)
 con2 <- dbConnect(odbc::odbc(), "reproreplica", timeout = 10)
 
-
+## TABLES
 dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPCODIGO=73")
 
+## PRODUCTS
 dbGetQuery(con2,"SELECT * FROM TBPPRODU WHERE TBPCODIGO=73") %>% View()
+
+## CLIENTS
+dbGetQuery(con2,"SELECT * FROM CLITBP WHERE TBPCODIGO=73") %>% View()
