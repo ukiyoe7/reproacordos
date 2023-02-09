3d1b3e99914b8ded3d9c3687ed56a71b71da7fc6
Sandro Jk <sandro007@gmail.com>
2023-02-09 10:53:48 -0300

    ok

---
+++ ANALISE_COMERCIAL/ANALISE_COMERCIAL_1.R
@@ -0,0 +1,30 @@
+library(DBI)
+library(tidyverse)
+library(googlesheets4)
+
+con2 <- dbConnect(odbc::odbc(), "reproreplica")
+
+cli <- dbGetQuery(con2,"SELECT DISTINCT C.CLICODIGO,
+                           CLINOMEFANT,
+                            CLIRAZSOCIAL,
+                             CLICNPJCPF,
+                              REPLACE(
+                               REPLACE(
+                                REPLACE(CLICNPJCPF,'.',''),'/',''),'-','') CNPJ,
+                                   C.GCLCODIGO,
+                                    GCLNOME,  
+                                     IIF(C.GCLCODIGO IS NULL,C.CLICODIGO || ' ' || CLINOMEFANT,'G' || C.GCLCODIGO || ' ' || GCLNOME) CLIENTE,
+                                      SETOR
+                                       FROM CLIEN C
+                                        LEFT JOIN (SELECT CLICODIGO,E.ZOCODIGO,CIDNOME,ZODESCRICAO SETOR,ENDCODIGO FROM ENDCLI E
+                                         LEFT JOIN CIDADE CID ON E.CIDCODIGO=CID.CIDCODIGO
+                                          LEFT JOIN (SELECT ZOCODIGO,ZODESCRICAO FROM ZONA WHERE ZOCODIGO IN (20,21,22,23,24,25,26,27,28))Z ON E.ZOCODIGO=Z.ZOCODIGO WHERE ENDFAT='S')A ON C.CLICODIGO=A.CLICODIGO
+                                           LEFT JOIN GRUPOCLI GR ON C.GCLCODIGO=GR.GCLCODIGO
+                                            WHERE CLICLIENTE='S'")
+
+
+
+cli %>% 
+   group_by(CLIRAZSOCIAL,CLINOMEFANT,CNPJ,GCLCODIGO) %>% 
+        summarize(C=n_distinct(CNPJ)) %>%
+   filter(is.na(GCLCODIGO)) %>% View()
